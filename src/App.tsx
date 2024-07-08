4fcdd6f6e8355263dd54b5ae8a94655d289bcc5b
Rosa Maggi <rosa.maggi@epfl.ch>
2024-07-08 12:01:00 +0200

    [Refactor] eliminate dead code

--- src/App.tsx
+++ src/App.tsx
@@ -2,48 +2,39 @@ import React from 'react'
 import { Base } from './epfl-elements/Base.tsx'
 import { Avatar } from './epfl-elements/Avatar.tsx'
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-import BioHazard from './pages/biohazard';
 import HomePage from './pages/homepage';
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Base>
-                         	<Base.AsideMenu>
-                         		<ul>
-			 	      	<li className="active"><a href="#">Hazards</a></li>
-			 	      	<li>
-			 	      		<ul>
-			 	      			<li>
-			 	      				<Link to="/">Home</Link>
-			 	      			</li>
-			 	      			<li>
-			 	      				<Link to="/biohazard">Bio hazards</Link>
-			 	      			</li>
-			 	      		</ul>
-			 	      	</li>
-                         		</ul>
-                         	</Base.AsideMenu>
-                         	<Base.Breadcrumbs><li className="breadcrumb-item">Laboratory Hazards Directory</li></Base.Breadcrumbs>
-                         	<Base.User>
-                         		<Avatar>
-                         			<Avatar.Image peopleSciper={ "169419" } />
-                         			<p>{ oidc.idToken.given_name } { oidc.idToken.family_name }</p>
-                         		</Avatar>
-                         	</Base.User>
+				<Base.AsideMenu>
+					<ul>
+						<li className="active"><a href="#">Hazards</a></li>
+						<li>
+							<ul>
+								<li>
+									<Link to="/">Home</Link>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</Base.AsideMenu>
+				<Base.Breadcrumbs><li className="breadcrumb-item">Laboratory Hazards Directory</li></Base.Breadcrumbs>
+				<Base.User>
+					<Avatar>
+					</Avatar>
+				</Base.User>
 
-                         	<div className="container-full" style={{width: "100%", padding: "1em" }}>
+				<div className="container-full" style={{width: "100%", padding: "1em" }}>
 
 					<Switch>
-						<Route path="/biohazard">
-							<NothingHereNow />
-						</Route>
 						<Route path="/">
 							<HomePage />
 						</Route>
 					</Switch>
 
-                         	</div>
+				</div>
 			</Base>
 		</BrowserRouter>
 	);
